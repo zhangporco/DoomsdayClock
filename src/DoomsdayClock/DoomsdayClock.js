0db8bc30d166d5c36bd0480d04f19ebccd84eb6c
Porco Zhang <zhangchao@cc1st.com>
2017-02-22 05:13:22 +0800

    initialization

---
+++ src/DoomsdayClock/DoomsdayClock.js
@@ -0,0 +1,18 @@
+/**
+ * 末日钟
+ * Created by porco on 2/17/17.
+ */
+(function (global, factory) {
+    console.log(12);
+    var app = new Vue({
+        el: '#app',
+        data: {
+            message: 'Hello Vue!'
+        }
+    })
+}(this, (function () { 'use strict';
+
+
+})));
+
+
